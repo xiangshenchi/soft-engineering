5be5bf8349689dfd45b2c2ff4fe933664e037bdf
shawn <3057098742@qq.com>
2024-09-09 23:28:50 +0800

    [add] 实验一代码:三角形测试

---
+++ SEproject1/src/main/java/org/example/TriangleTest.java
@@ -0,0 +1,134 @@
+//package org.example;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//
+//class TriangleTest {
+//    @Test
+//    @DisplayName("输入错误")
+//    void parameters_error_test() {
+//        Triangle triangle = new Triangle();
+//        String type = triangle.classify(0, 4, 5);
+//        assertEquals("输入错误", type);
+//    }
+//    @Test
+//    @DisplayName("不等边三角形")
+//    void scalene_test() {
+//        Triangle triangle = new Triangle();
+//        String type = triangle.classify(3, 4, 6);
+//        assertEquals("不等边三角形", type);
+//    }
+//}
+
+
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.text.ParseException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TriangleTest {
+
+    @Test
+    @DisplayName(value="不等边三角形")
+    void testTriangle() {
+        Triangle triangle = new Triangle();
+
+        String type = triangle.classify(3, 4, 6);
+        assertEquals("不等边三角形", type);
+    }
+
+    @Test
+    @DisplayName(value="等边三角形")
+    void equallaterialTriangle() {
+        Triangle triangle = new Triangle();
+
+        String type = triangle.classify(3, 3, 3);
+        assertEquals("等边三角形", type);
+    }
+
+    @Test
+    @DisplayName(value="非三角形")
+    void notTriangle() {
+        Triangle triangle = new Triangle();
+
+        String type = triangle.classify(3, 3, 6);
+        assertEquals("非三角形", type);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "3,4,6,不等边三角形",
+            "3,3,3,等边三角形",
+            "3,3,6,非三角形"
+    })
+    void paramTriangle(int a, int b,int c,String expected) {
+        Triangle triangle = new Triangle();
+
+        String type = triangle.classify(a, b, c);
+
+        assertEquals(expected, type);
+    }
+    @DisplayName(value="三角形一般边界测试用例")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/三角形一般边界测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
+    void fileTest(int a, int b,int c,String expected) throws ParseException {
+
+        Triangle triangle=new Triangle();
+
+        String type = triangle.classify(a, b, c);
+
+        assertEquals(expected, type);
+    }
+    @DisplayName(value="三角形健壮性最坏情况测试用例")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/三角形健壮性最坏情况测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
+    void fileTest1(int a, int b, int c, String expected) throws ParseException {
+
+        Triangle triangle=new Triangle();
+
+        String type = triangle.classify(a, b, c);
+
+        assertEquals(expected, type);
+    }
+    @DisplayName(value="三角形健壮测试用例")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/三角形健壮测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
+    void fileTest2(int a, int b, int c, String expected) throws ParseException {
+
+        Triangle triangle=new Triangle();
+
+        String type = triangle.classify(a, b, c);
+
+        assertEquals(expected, type);
+    }
+    @DisplayName(value="三角形弱一般等价类测试用例")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/三角形弱一般等价类测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
+    void fileTest3(int a, int b, int c, String expected) throws ParseException {
+
+        Triangle triangle=new Triangle();
+
+        String type = triangle.classify(a, b, c);
+
+        assertEquals(expected, type);
+    }
+    @DisplayName(value="三角形最坏情况测试用例")
+    @ParameterizedTest
+    @CsvFileSource(resources = "/三角形最坏情况测试用例.csv",numLinesToSkip =1,encoding = "UTF-8")
+    void fileTest4(int a, int b, int c, String expected) throws ParseException {
+
+        Triangle triangle=new Triangle();
+
+        String type = triangle.classify(a, b, c);
+
+        assertEquals(expected, type);
+    }
+}
